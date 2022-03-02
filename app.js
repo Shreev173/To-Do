0b1e4c434cfd3bc6d1175e5e87c6ba2c28673701
Shreeshan <shreev173@gmail.com>
2022-03-02 20:57:08 +0545

    completed

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
+const filterOption = document.querySelector(".filter-todo");
 
 //Functions
 
@@ -71,8 +72,54 @@ const deleteCheck = (e) => {
   }
 };
 
+const filterTodo = (e) => {
+  const todos = todoList.childNodes;
+  // console.log(todos);
+
+  todos.forEach(function (todo) {
+    switch (e.target.value) {
+      case "all":
+        todo.style.display = "flex";
+        break;
+
+      case "completed":
+        if (todo.classList.contains("completed")) {
+          todo.style.display = "flex";
+        } else {
+          todo.style.display = "none";
+        }
+        break;
+
+      case "uncompleted":
+        if (!todo.classList.contains("completed")) {
+          todo.style.display = "flex";
+        } else {
+          todo.style.display = "none";
+        }
+
+        break;
+    }
+  });
+};
+
+// const saveLocalTodos = (todo) => {
+//   //sth in there
+
+//   let todos;
+
+//   if (localStorage.getItem("todos") === null) {
+//     todos = [];
+//   } else {
+//     todos.JSON.parse(localStorage.getItem("todos"));
+//   }
+
+//   todos.push(todo);
+// };
+
 //Event listeners
 
 todoButton.addEventListener("click", addTodo);
 
 todoList.addEventListener("click", deleteCheck);
+
+filterOption.addEventListener("click", filterTodo);
